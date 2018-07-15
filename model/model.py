579e6a770e9242142d5c72fd2ad10b507138ac2c
SunQ <sunq0313@gmail.com>
2018-07-15 23:04:43 +0800

    add U-net structure

--- model/model.py
+++ model/model.py
@@ -1,13 +1,15 @@
 import sys 
-sys.path.append('./')
-from base import BaseModel
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+sys.path.append('./')
+from base import BaseModel
+
 
 
 class BasicBlock(nn.Module):
 
-    def __init__(self, in_ch, ch, stride=1, downsample=None):
+    def __init__(self, in_ch, ch, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_ch, ch, 3, stride, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(ch)
@@ -15,14 +17,11 @@ class BasicBlock(nn.Module):
 
         self.conv2 = nn.Conv2d(ch, ch, 3, 1, 1, bias=False)
         self.bn2 = nn.BatchNorm2d(ch)
-        self.downsample = downsample
 
     def forward(self, x_input):
         residual = x_input
         x = self.relu(self.bn1(self.conv1(x_input)))
         x = self.bn2(self.conv2(x))
-        if self.downsample is not None:
-            residual = self.downsample(residual)
         return x + residual
 
 
@@ -49,93 +48,109 @@ class BottleneckBlock(nn.Module):
         return x + residual
 
 
-class ResNet6(BaseModel):
+class ResNet(BaseModel):
 
-    def __init__(self, in_ch=3, out_ch=3, ch=16):
-        super(ResNet6, self).__init__()
+    def __init__(self, num_layers, in_ch=3, out_ch=3, ch=16):
+        super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(in_ch, 32, 7, stride=1, padding=3, bias=False)
         self.inorm1 = nn.InstanceNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=0, bias=False)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=1, bias=False)
         self.inorm2 = nn.InstanceNorm2d(64)
-        # self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=0, bias=False)
-        # self.inorm3 = nn.InstanceNorm2d(128)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=False)
+        self.inorm3 = nn.InstanceNorm2d(128)
 
-        self.block1 = BasicBlock(64, 64)
-        self.block2 = BasicBlock(64, 64)
-        self.block3 = BasicBlock(64, 64)
-        self.block4 = BasicBlock(64, 64)
-        self.block5 = BasicBlock(64, 64)
-        self.block6 = BasicBlock(64, 64)
+        self.res_blocks = self._make_layers(BasicBlock, num_layers, 128)
 
-        # self.conv4_t = nn.ConvTranspose2d(128, 64, 3, 2, padding=0, bias=False)
-        # self.inorm4 = nn.InstanceNorm2d(64)
-        self.conv5_t = nn.ConvTranspose2d(64, 32, 4, 2, padding=0, bias=False)
+        # upsample
+        self.conv4 = nn.Conv2d(128, 64, 3, 1, padding=1, bias=False)
+        self.inorm4 = nn.InstanceNorm2d(64)
+        self.conv5 = nn.Conv2d(64, 32, 3, 1, padding=1, bias=False)
         self.inorm5 = nn.InstanceNorm2d(32)
         self.conv6 = nn.Conv2d(32, out_ch, 7, 1, padding=3, bias=False)
         self.inorm6 = nn.InstanceNorm2d(out_ch)
 
+    def _make_layers(self, block, num_layers, num_channels):
+        layers = [block(num_channels, num_channels)]*num_layers
+        return nn.Sequential(*layers)
 
     def forward(self, x_input):
         x = F.relu(self.inorm1(self.conv1(x_input)))
         x = F.relu(self.inorm2(self.conv2(x)))
-        # x = F.relu(self.inorm3(self.conv3(x)))
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        x = self.block5(x)
-        x = self.block6(x)
-        # x = F.relu(self.inorm4(self.conv4_t(x)))
-        x = F.relu(self.inorm5(self.conv5_t(x)))
-        x = F.tanh(self.inorm6(self.conv6(x)))
+        x = F.relu(self.inorm3(self.conv3(x)))
+
+        x = self.res_blocks(x)
+
+        x = F.relu(self.inorm4(self.conv4(F.upsample(x, scale_factor=2))))
+        x = F.relu(self.inorm5(self.conv5(F.upsample(x, scale_factor=2))))
+        x = (F.tanh(self.inorm6(self.conv6(x))) + 1)/2
         return x
 
-class ResNet9(BaseModel):
 
-    def __init__(self, in_ch=3, out_ch=3, ch=16):
-        super(ResNet9, self).__init__()
-        self.conv1 = nn.Conv2d(in_ch, 32, 7, stride=1, padding=3, bias=False)
-        self.inorm1 = nn.InstanceNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=0, bias=False)
-        self.inorm2 = nn.InstanceNorm2d(64)
-        # self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=0, bias=False)
-        # self.inorm3 = nn.InstanceNorm2d(128)
+class Unet(nn.Module):
 
-        self.block1 = BasicBlock(64, 64)
-        self.block2 = BasicBlock(64, 64)
-        self.block3 = BasicBlock(64, 64)
-        self.block4 = BasicBlock(64, 64)
-        self.block5 = BasicBlock(64, 64)
-        self.block6 = BasicBlock(64, 64)
-        self.block7 = BasicBlock(64, 64)
-        self.block8 = BasicBlock(64, 64)
-        self.block9 = BasicBlock(64, 64)
+    def __init__(self, in_ch, out_ch, n_features=32):
+        super(Unet, self).__init__()
+        
+        self.encoder = nn.Sequential(
+            self._make_block(in_ch,        n_features),
+            self._make_block(n_features  , n_features*2),
+            self._make_block(n_features*2, n_features*4),
+            self._make_block(n_features*4, n_features*8),
+        )
+        
+        self.bottleneck = nn.Sequential(*[
+            self._make_block(n_features*8,  n_features*16),
+            self._make_block(n_features*16, n_features*16),
 
-        # self.conv4_t = nn.ConvTranspose2d(128, 64, 3, 2, padding=0, bias=False)
-        # self.inorm4 = nn.InstanceNorm2d(64)
-        self.conv5_t = nn.ConvTranspose2d(64, 32, 4, 2, padding=0, bias=False)
-        self.inorm5 = nn.InstanceNorm2d(32)
-        self.conv6 = nn.Conv2d(32, out_ch, 7, 1, padding=3, bias=False)
-        self.inorm6 = nn.InstanceNorm2d(out_ch)
+            # nn.ConvTranspose2d(n_features*16, n_features*8, 2, 2),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(out_ch, out_ch // 2, 3, 1, padding=1),
+        ])
+        
+        self.decoder = nn.Sequential(
+            self._make_block(n_features*16, n_features*8, upsample=True),
+            self._make_block(n_features*8, n_features*4, upsample=True),
+            self._make_block(n_features*4, n_features*2, upsample=True),
+            self._make_block(n_features*2, n_features  , upsample=False, activation=False),
+        )
+        self.output = nn.Conv2d(n_features, out_ch, 1, 1)
+        
+
+    def _make_block(self, in_ch, out_ch, upsample=False, activation=True):
+        layers = [
+            nn.Conv2d(in_ch, out_ch, 3, 1, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(out_ch, out_ch, 3, 1, padding=1),
+            nn.BatchNorm2d(out_ch),
+        ]
+        if activation:
+            layers.append(nn.ReLU(inplace=True))
+        if upsample:
+            layers.append(nn.Upsample(scale_factor=2))
+            layers.append(nn.Conv2d(out_ch, out_ch // 2, 3, 1, padding=1))
+
+        return nn.Sequential(*layers)
 
 
-    def forward(self, x_input):
-        x = F.relu(self.inorm1(self.conv1(x_input)))
-        x = F.relu(self.inorm2(self.conv2(x)))
-        # x = F.relu(self.inorm3(self.conv3(x)))
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        x = self.block5(x)
-        x = self.block6(x)
-        x = self.block7(x)
-        x = self.block8(x)
-        x = self.block9(x)
-        # x = F.relu(self.inorm4(self.conv4_t(x)))
-        x = F.relu(self.inorm5(self.conv5_t(x)))
-        x = F.tanh(self.inorm6(self.conv6(x)))
+    def forward(self, x):
+        skip = []
+        for down_block in self.encoder.children():
+            x = down_block(x)
+            skip.append(x)
+            x = F.max_pool2d(x, 2)
+
+        x = self.bottleneck(x)
+
+        for up_block in self.decoder.children():
+            x = torch.cat([skip.pop(), x], dim=1)
+            x = up_block(x)
+
+        x = self.output(x)
+        x = (F.tanh(x) + 1)/2
         return x
+
 
 class Discriminator(BaseModel):
 
@@ -150,7 +165,7 @@ class Discriminator(BaseModel):
         self.conv3 = nn.Conv2d(128, 256, 4, 2, padding=1, bias=False)
         self.inorm3 = nn.InstanceNorm2d(256)
 
-        self.conv4 = nn.Conv2d(256, 512, 3, 1, padding=1, bias=False)
+        self.conv4 = nn.Conv2d(256, 512, 4, 2, padding=1, bias=False)
         self.inorm4 = nn.InstanceNorm2d(512)
 
         self.conv5 = nn.Conv2d(512,   1, 1, 1, padding=0, bias=False)
@@ -163,10 +178,10 @@ class Discriminator(BaseModel):
         x = F.leaky_relu_(self.inorm4(self.conv4(x)), 0.2)
         return F.sigmoid(self.conv5(x))
 
-class GAN_Res6(BaseModel):
-    def __init__(self, in_ch=3, out_ch=3, ch=16):
-        super(GAN_Res6, self).__init__()
-        self.gen = ResNet6(in_ch=in_ch, out_ch=out_ch, ch=ch)
+class GAN_ResNet(BaseModel):
+    def __init__(self, in_ch=3, out_ch=3, num_res=6, ch=16):
+        super(GAN_ResNet, self).__init__()
+        self.gen = ResNet(num_res, in_ch=in_ch, out_ch=out_ch, ch=ch)
         self.dis = Discriminator(in_ch=out_ch)
         
 
@@ -174,12 +189,14 @@ class GAN_Res6(BaseModel):
         fake_y = self.gen(x_input)
         return fake_y
 
-class GAN_Res9(BaseModel):
-    def __init__(self, in_ch=3, out_ch=3, ch=16):
-        super(GAN_Res9, self).__init__()
-        self.gen = ResNet9(in_ch=in_ch, out_ch=out_ch, ch=ch)
+
+class GAN_UNet(BaseModel):
+    def __init__(self, in_ch=3, out_ch=3, n_features=32):
+        super(GAN_UNet, self).__init__()
+        self.gen = Unet(in_ch=in_ch, out_ch=out_ch, n_features=n_features)
         self.dis = Discriminator(in_ch=out_ch)
         
+
     def forward(self, x_input, discrim=True):
         fake_y = self.gen(x_input)
         return fake_y
@@ -188,8 +205,10 @@ class GAN_Res9(BaseModel):
 class CycleGAN(BaseModel):
     def __init__(self):
         super(CycleGAN, self).__init__()
-        self.AtoB = GAN_Res9(in_ch=1, ch=64)
-        self.BtoA = GAN_Res6(out_ch=1, ch=32)
+        # self.AtoB = GAN_ResNet(in_ch=1, ch=64, num_res=9)
+        # self.BtoA = GAN_ResNet(out_ch=1, ch=64, num_res=9)
+        self.AtoB = GAN_UNet(in_ch=1,  n_features=32)
+        self.BtoA = GAN_UNet(out_ch=1, n_features=32)
 
     def forward(self, x, from_A=True):
         if from_A:
@@ -205,11 +224,16 @@ class CycleGAN(BaseModel):
 
 if __name__ == '__main__':
     import torch
-    gen = ResNet6()
-    dis = Discriminator()
+    # gen = ResNet()
+    # dis = Discriminator()
 
-    x_dummy = torch.randn((1, 3, 256, 256))
-    x_inter = gen(x_dummy)
-    print(x_inter.shape)
-    output = dis(x_inter)
+    x_dummy = torch.randn((4, 3, 256, 256))
+    # x_inter = gen(x_dummy)
+    # print(x_inter.shape)
+    # output = dis(x_inter)
+    # print(output.shape)
+
+    unet = Unet(3, 1, 32)
+    print(unet)
+    output = unet(x_dummy)
     print(output.shape)
